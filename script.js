2d6a78eda09ee9a2ae0cadaf9957c4a5c3cdb733
Clover <sewsistacomo@gmail.com>
2026-07-23 19:16:51 -0500

    adding something to js file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // so much empty
+// Wait for the page to load
+window.addEventListener("load", function () {
+   // Make the GET request   
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+       .then(function (response) {
+           // Convert the response to JSON
+           return response.json();
+      })
+       .then(function (json) {
+           console.log(json); // Log the JSON data
+       })
+       .catch(function (error) {
+           console.error("Error fetching the data:", error);
+       });
+});
